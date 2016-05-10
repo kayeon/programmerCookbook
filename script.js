c3cfacc5b67e41451054ac77105dbe42a2aa6426
kayeon <kayeon@gmail.com>
2016-05-09 20:04:46 -0700

    single quote change

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ data = require('./recipe.json');
 recipes = data.recipes;
 first_recipe = recipes[0];
 
-console.log("Listing ingredients for " + first_recipe.name);
+console.log('Listing ingredients for ' + first_recipe.name);
 
-console.log("Recipe has " + first_recipe.ingredients.length + " ingredients");
+console.log('Recipe has ' + first_recipe.ingredients.length + ' ingredients');
 
 first_recipe.ingredients.forEach(function(ingredient, index) {
-  console.log("Ingredient " + (index + 1) + ": " + ingredient.quantity + " of " + ingredient.name);
+  console.log('Ingredient ' + (index + 1) + ': ' + ingredient.quantity + ' of ' + ingredient.name);
 });
 
 for (s in first_recipe.steps) {
