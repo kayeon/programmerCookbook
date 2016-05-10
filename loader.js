5bd222d0b26d42d40eb8896a54e23ee79dd52743
kayeon <kayeon@gmail.com>
2016-05-09 21:00:37 -0700

    updated single quotes

--- loader.js
+++ loader.js
@@ -1,11 +1,11 @@
 // Helper function to load JSON files over the network.
 function loadJSON(filename, callback) {
   var request = new XMLHttpRequest();
-  request.overrideMimeType("application/json");
+  request.overrideMimeType('application/json');
   request.open('GET', filename, true);
   request.onreadystatechange = function() {
     // TODO: Magic number. What is "4"?
-    if (request.readyState == 4 && request.status == "200") {
+    if (request.readyState == 4 && request.status == '200') {
       // Required use of an anonymous callback as .open will NOT return a value
       // but simply returns undefined in asynchronous mode
       callback(JSON.parse(request.responseText));
